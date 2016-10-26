a0c1cd5918c7e4648ee217e65a7a875861c0b37d
Brandon Fancher <brandonfancher@gmail.com>
2016-10-26 18:16:03 -0400

    Adding Event page listing RSVPs.

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router';
+import { getRsvps } from '../api/api';
+import '../styles/Event.css';
 
-const Event = () => (
-  <div>
-    <h2>Event</h2>
-  </div>
-)
+export default class Event extends React.Component {
 
-export default Event;
+  static propTypes = {
+    params: React.PropTypes.object.isRequired,
+    pathname: React.PropTypes.string.isRequired,
+  }
+
+  state = {
+    rsvps: [],
+  }
+
+  componentWillMount() {
+    const { params } = this.props;
+    getRsvps(params.eventId)
+      .then(res => res.data)
+      .then(rsvps => this.setState({ rsvps }))
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    const { rsvps } = this.state;
+    const { pathname } = this.props;
+    return (
+      <div className="event">
+        <div className="rsvp-list">
+          <h2>RSVPs</h2>
+          {rsvps.map(rsvp => (
+            <Link to={`${pathname}/member/${rsvp.member.id}`} key={rsvp.member.id}>
+              <p>{rsvp.member.name}</p>
+            </Link>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
