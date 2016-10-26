43392e2190507fcc7cb9a7f2e48dfb25aa65f2f8
Brandon Fancher <brandonfancher@gmail.com>
2016-10-26 16:35:24 -0400

    Adding home styles. Adding Events list and styles.

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router'
+import { getEvents } from '../api/api';
+import '../styles/Events.css';
 
-const Events = () => (
-  <div>
-    <h2>Events</h2>
-  </div>
-)
+export default class Events extends React.Component {
 
-export default Events;
+  state = {
+    events: [],
+  }
+
+  componentWillMount() {
+    getEvents()
+      .then(res => res.data)
+      .then(events => this.setState({ events }))
+      .catch(error => console.log(error));
+  }
+
+  formatDate = (time) => {
+    const months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'Jun.', 'Jul.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
+    const daysOfWeek = ['Sun.', 'Mon.', 'Tues.', 'Wed.', 'Thur.', 'Fri.', 'Sat.'];
+    const date = new Date(time);
+    const day = date.getDate();
+    const dayOfWeekIndex = date.getDay();
+    const monthIndex = date.getMonth();
+    const year = date.getFullYear();
+    return `${daysOfWeek[dayOfWeekIndex]}, ${months[monthIndex]} ${day}, ${year}`;
+  }
+
+  render() {
+    const { events } = this.state;
+    return (
+      <div>
+        <h2>Events</h2>
+        {events.reverse().map(event => (
+          <div key={event.id} className="event-list">
+            <div className="event-name-col">
+              <Link to={`event/${event.id}`}>{event.name}</Link>
+            </div>
+            <div className="event-time-col">{this.formatDate(event.time)}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
